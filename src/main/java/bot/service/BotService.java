eaf8dd663ec5c58dd4684477b1ceddb85d4920ae
erik <iqboy118@gmail.com>
2018-03-23 23:32:02 +0800

    增加redis计数功能

--- src/main/java/bot/service/BotService.java
+++ src/main/java/bot/service/BotService.java
@@ -1,7 +1,8 @@
 package bot.service;
 
 import bot.entity.QaEntity;
-import com.google.common.collect.Maps;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -11,17 +12,26 @@ import java.util.Map;
 @Service
 public class BotService {
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
     public List<Map> getQuestions(String words) {
         return Collections.EMPTY_LIST;
     }
 
     public QaEntity getAnswer(String idx) {
         // 增加计数
+        String count = redisTemplate.opsForValue().get(getAccessCount(idx));
+        Integer total = Integer.parseInt(count == null ? "0" : count);
 
         // 返回答案
         QaEntity mock = new QaEntity();
         mock.setIndex(1);
-        mock.setAnswer("answer");
+        mock.setAnswer(count);
         return mock;
+    }
+
+    private String getAccessCount(String idx) {
+        return String.format("count:%s", idx);
     }
 }
